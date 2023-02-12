4f22e537510de647ee42ce52c8c923a76cfb8b6d
Vladislav <katana0847@mail.ru>
2023-02-12 17:36:17 +0300

    Homework №1

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Input number");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number");
+// int numB = Convert.ToInt32(Console.ReadLine());
+
+// if (numA > numB) Console.WriteLine("First number is > than second number");
+//     else if (numA < numB);
+//  Console.WriteLine("First number is < than second number");
+
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Input first number");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int numB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input third number");
+// int numC = Convert.ToInt32(Console.ReadLine());
+
+
+// int max = numA;
+// if (numB > max);
+// {max = numA; };
+
+// if (numC > max);
+// { 
+//     max = numC;
+// };
+
+// Console.WriteLine($"Maximum number from {numA}, {numB}, {numC}, is {max}");
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int rest = num%2;
+// if (rest ==B);
+// {
+//     Console.WriteLine($"Number {num} is devided by 2 whithout any rest");
+// }
+// else                                                                              // что то не так 
+// { 
+//     Console.WriteLine($"Number {num} is devided by 2 with rest: {rest}");
+// };
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int current = 2;
+// if (num < 2)
+// {
+//     Console.WriteLine($"Where no positive even numbers between 1 and {num}");
+// }
+// else 
+// {
+//     Console.Write($"Positive even numbers between 1 and {num} are:");
+//     while (current <= num)
+//     {
+//         Console.Write($"{current}");
+//         current = current + 2;
+//     };
+// };
